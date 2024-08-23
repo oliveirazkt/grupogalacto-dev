f8ce6bd79916ada53f282cdc6617588d7e7d7f84
thiago <thiagomanoel.ux@gmail.com>
2024-08-23 16:58:52 -0300

    finalização de cards

---
+++ app/(routes)/(index)/componets/cards.js
@@ -0,0 +1,50 @@
+import { useState, useEffect, useRef } from "react";
+import { motion } from 'framer-motion'
+import "./cards.css"
+
+const empresas = [
+    { logo: "./Logo-drop2.svg", descricao: "Na DropNexus, transformamos suas ideias em negócios lucrativos. Oferecemos tudo o que você precisa para começar e escalar sua loja online sem complicações." },
+    { logo: "./Logo-galac.svg", descricao: "Descubra um mundo de praticidade e inovação na Utilidades Premium. Temos tudo para facilitar o seu dia a dia, desde itens para a casa até ferramentas essenciais" },
+    { logo: "./Logo-doce.svg", descricao: "Você está prestes a entrar no mundo dos sabores mais irresistíveis! Na Doçura Infinita, cada mordida é uma explosão de felicidade." },
+    { logo: "./Logo-drop2.svg", descricao: "Na DropNexus, transformamos suas ideias em negócios lucrativos. Oferecemos tudo o que você precisa para começar e escalar sua loja online sem complicações." },
+    { logo: "./Logo-galac.svg", descricao: "Descubra um mundo de praticidade e inovação na Utilidades Premium. Temos tudo para facilitar o seu dia a dia, desde itens para a casa até ferramentas essenciais" },
+    { logo: "./Logo-doce.svg", descricao: "Você está prestes a entrar no mundo dos sabores mais irresistíveis! Na Doçura Infinita, cada mordida é uma explosão de felicidade." },
+
+]
+
+export default function Cards() {
+    
+    const carousel = useRef();
+    const [width, setWidth] = useState(0)
+    
+    useEffect(() => {
+      console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+      setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth )
+  
+    }, [])
+  
+    return (
+
+    <div className="container Logo">
+    <motion.div ref={carousel} className="carousel" whileTap={{ cursor:"grabbing"}}>
+      <motion.div
+      className="inner"
+        drag="x"
+        dragConstraints={{ right: 0, left: -width }}
+       
+      >
+        {empresas.map((e, i) => (
+            <motion.div className="item" key={i}>
+                <div className="card-item">
+                    <img src={e.logo} alt="Texto alt" />
+                    <p>
+                        {e.descricao}
+                    </p>
+                </div>
+          </motion.div>
+        ))}
+    </motion.div>
+    </motion.div>
+  </div>
+  )
+}
--- app/(routes)/(index)/page.js
+++ app/(routes)/(index)/page.js
@@ -1,71 +1,132 @@
 "use client";
+
 import "./style.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import SwiperCore, { Autoplay } from 'swiper';
+SwiperCore.use([Autoplay]);
 import Header from "../../../componets/header";
+import Cards from "./componets/cards";
+
+const images = [
+  "./Logo-doce.svg",
+  "./Logo-drop2.svg",
+  "./Logo-galac.svg",
+  "./Logo-doce.svg",
+  "./Logo-drop2.svg",
+  "./Logo-galac.svg",
+  "./Logo-drop2.svg",
+  "./Logo-galac.svg",
+];
 
 export default function Page() {
+  
+
   return (
     <div id="section-header">
       <Header />
       <div id="banner">
         <img src="/Logo.branca.svg" alt="Logomarca" />
         <p className="paragrafo-gupo">
-          O Grupo Galacto é uma conglomerado de empresas que <br/>
-          opera em diferentes setores, englobando diversas filiais <br/>
+          O Grupo Galacto é uma conglomerado de empresas que <br />
+          opera em diferentes setores, englobando diversas filiais <br />
           especializadas em áreas específicas
         </p>
         <div>
           <button className="button-Loja">Veja nossas Lojas </button>
-          <button className="button-entrar"> Entra em contato com nosco </button>
-          </div>
+          <button className="button-entrar">
+            {" "}
+            Entra em contato com nosco{" "}
+          </button>
+        </div>
       </div>
-      <div className="container" id="sobre" >
-        <img className="imagem-retangular" src="/Mask group.svg" alt="Logomarca" />
+      <div className="container" id="sobre">
+        <img
+          className="imagem-retangular"
+          src="/Mask group.svg"
+          alt="Logomarca"
+        />
         <div>
-          <h1> Sobre nos </h1>  
+          <h1> Sobre nos </h1>
           <p>
-            
-            O Grupo Galacto é uma conglomerado de empresas que <br/>
-            opera em diferentes setores, englobando diversas filiais<br/>
+            O Grupo Galacto é uma conglomerado de empresas que <br />
+            opera em diferentes setores, englobando diversas filiais
+            <br />
             especializadas em áreas específicas
           </p>
-          <div id="number"> 
+          <div id="number">
             <div>
-              <h1>
-              11.5M+
-              </h1>
-              <p>
-                Produtos vendidos
-              </p>
+              <h1>11.5M+</h1>
+              <p>Produtos vendidos</p>
             </div>
             <div>
-              <h1>
-              21.9M
-              </h1>
-              <p>
-                clientes  satisfeitos
-              </p>
+              <h1>21.9M</h1>
+              <p>clientes satisfeitos</p>
             </div>
           </div>
-          
         </div>
       </div>
       <div className="container Logo">
-          <img src="/Logo-doce.svg" alt="Logo Doce" />
-          <img src="/Logo-drop2.svg" alt="Logo Doce" />
-          <img src="/Logo-galac.svg" alt="Logo Doce" />
-          <img src="/Logo-doce.svg" alt="Logo Doce" />
-          <img src="/Logo-drop2.svg" alt="Logo Doce" />
-          <img src="/Logo-doce.svg" alt="Logo Doce" />
-          <img src="/Logo-drop2.svg" alt="Logo Doce" />
-          <img src="/Logo-galac.svg" alt="Logo Doce" />
-          <img src="/Logo-doce.svg" alt="Logo Doce" />
-          <img src="/Logo-drop2.svg" alt="Logo Doce" />
+      <Swiper
+      spaceBetween={10}
+          slidesPerView={7}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+        >
+          {images.map((image, index) => (
+              <SwiperSlide>
+              <img
+                    src={image}
+                    alt={`Slide ${index}`}
+                    style={{ width: "100%" }}
+            />
+            </SwiperSlide>
+                
+              
+            ))}
+          
+      {/* <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide> */}
+      ...
+    </Swiper>
+        {/* <motion.div
+          ref={carousel}
+          className="carousel"
+          whileTap={{ cursor: "grabbing" }}
+          
+        >
+          <motion.div
+            className="inner"
+            drag="x"
+            dragConstraints={{ right: 0, left: 0 }}
+            animate={{ x: ["0%", "-50%" ] }}
+            
+          >
+            {items.map((image, index) => (
+              <motion.div
+                className="item"
+                key={index}
+                
+              >
+                <img
+                  src={image}
+                  alt={`Slide ${index}`}
+                  style={{ width: "100%" }}
+                />
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div> */}
       </div>
       <div className="container titulo">
-        <h1>
-        Nossas  Lojas
-        </h1>
-        
+        <h1>Nossas Lojas</h1>
+        <Cards />
       </div>
     </div>
   );
